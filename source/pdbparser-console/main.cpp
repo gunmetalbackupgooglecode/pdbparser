454dfdb9cd996c79990a063c33793ee88845cfc9
Ivan Kulikov <gunmetal313@gmail.com>
2015-03-15 18:32:10 +0300

    init main

--- source/pdbparser-console/main.cpp
+++ source/pdbparser-console/main.cpp
@@ -3,18 +3,21 @@
 #include <fstream>
 #include <string>
 #include "optionparser.h"
+#include <memory>
 
 #include <template-version.h>
+#include <symenglib/symenglib_api.h>
+#include <symenglib/msf.h>
 
 enum optionIndex {
-    UNKNOWN, HELP
+    pdbparser, HELP, VERBOSE
 };
 
 const option::Descriptor usage[] = {
-    {UNKNOWN, 0, "", "", option::Arg::None, "USAGE: pdbparser-console [options]\n\n"
-        "Options:"},
-    {HELP, 0, "", "help", option::Arg::None, "  --help  \tPrint usage and exit."},
-    {UNKNOWN, 0, "", "", option::Arg::None, "\nExamples:\n"
+    {pdbparser, 0, "", "", option::Arg::None, "USAGE: pdbparser-console [options]\n\n" "Options:"},
+    { VERBOSE, 0, "v", "verbose", option::Arg::None, 0}, // VERBOSE (counted option)
+    {HELP, 0, "h", "help", option::Arg::None, "  --help  \tPrint usage and exit."},
+    {pdbparser, 0, "", "", option::Arg::None, "\nExamples:\n"
         " pdbparser-console [path-to-pdb-file] \n"
         " "},
     {0, 0, 0, 0, 0, 0}
@@ -31,17 +34,27 @@ int main(int argc, char* argv[]) {
     if (parse.error())
         return 1;
 
+    if (options[VERBOSE]) {
+        std::cout << "Version: " << TEMPLATE_VERSION << std::endl;
+        std::cout << "pdbparser_GIT_SHA1: " << TEMPLATE_GIT_SHA1 << std::endl;
+        std::cout << "pdbparser_GIT_REV: " << TEMPLATE_GIT_REV << std::endl;
+        return 0;
+    }
+
     if (options[HELP] || argc == 0) {
         option::printUsage(std::cout, usage);
         return 0;
     }
-
-    std::cout << "Version: " << TEMPLATE_VERSION << std::endl;
-    std::cout << "pdbparser_GIT_SHA1: " << TEMPLATE_GIT_SHA1 << std::endl;
-
-
-
-
+    
+    for (int i = 0; i < parse.nonOptionsCount(); ++i)
+        std::cout << "Non-option #" << i << ": " << parse.nonOption(i) << "\n";
+    
+    std::shared_ptr<symenglib::MSF> msf(new symenglib::MSF());
+    
+    
+    
+    
+    
     delete[] options;
     delete[] buffer;
 }
