ccad2224002aeecad4f414435da57d5400d5fc2f
Ivan Kulikov <gunmetal313@gmail.com>
2015-03-15 18:51:11 +0300

    CMake: CLang. type was fixed

--- cmake/PlatformLinuxCLang.cmake
+++ cmake/PlatformLinuxCLang.cmake
@@ -1,5 +1,5 @@
 message(STATUS "Configuring for platform Linux/CLang.")
 
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
