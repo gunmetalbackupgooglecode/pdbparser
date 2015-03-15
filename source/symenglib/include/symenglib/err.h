cbeaea9005670986f9d35e840a1e554a9e4c06f6
Ivan Kulikov <gunmetal313@gmail.com>
2015-03-15 18:31:15 +0300

    err: SYMENGLIB_Exception was added

--- source/symenglib/include/symenglib/err.h
+++ source/symenglib/include/symenglib/err.h
@@ -1,6 +1,18 @@
-#pragma once
+#ifndef SYMENGLIB_ERR_H
+#define SYMENGLIB_ERR_H
+
+#include <types.h>
+#include <symenglib/symenglib_api.h>
+#include <exception>
+
+class SYMENGLIB_API SYMENGLIB_Exception : std::exception
+{ };
 
 
 
-///////////////////////////////////////////////////////////////////////////////
 
+
+
+
+
+#endif // SYMENGLIB_ERR_H
