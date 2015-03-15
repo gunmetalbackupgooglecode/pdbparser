bd212d20790c21bd641a903589d18624f2073834
Ivan Kulikov <gunmetal313@gmail.com>
2015-03-15 18:43:30 +0300

    Cmake: try fix CLang error

--- cmake/PlatformLinuxCLang.cmake
+++ cmake/PlatformLinuxCLang.cmake
@@ -1,1 +1,5 @@
-message(STATUS "Configuring for platform Linux/CLang.") 
+message(STATUS "Configuring for platform Linux/CLang.")
+
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
