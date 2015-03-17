c88ac271fde12caf7e47554598d220d00006f684
Ivan Kulikov <gunmetal313@gmail.com>
2015-03-17 12:26:15 +0300

    msf: fix type tellg error

--- source/symenglib/source/msf.cpp
+++ source/symenglib/source/msf.cpp
@@ -13,8 +13,10 @@
 namespace symenglib {
 
     void MSF::Open(const std::string & file) {
-        currentPdbFile.open(file, std::ios::binary | std::ios::ate);
-        if (currentPdbFile.tellg()<sizeof (MSF_HDR)) {
+        currentPdbFile.open(file, std::ios::binary);
+        currentPdbFile.seekg(0,std::ios::end);
+        u64 nSize = static_cast<u64>(currentPdbFile.tellg());
+        if (nSize<sizeof (MSF_HDR)) {
             throw symenglib::SYMENGLIB_BAD_PDB_FILE();
         }
         MSF_HDR msf_hdr;
